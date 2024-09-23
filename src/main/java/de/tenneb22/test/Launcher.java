59f23dd88ecc4a1c1f130fac06f02b9a27c95f7d
Tenneb22 <126985099+Tenneb22@users.noreply.github.com>
2024-09-23 17:54:44 +0200

    WindowManager

--- src/main/java/de/tenneb22/test/Launcher.java
+++ src/main/java/de/tenneb22/test/Launcher.java
@@ -1,9 +1,18 @@
 package de.tenneb22.test;
 
+import de.tenneb22.core.WindowManager;
 import org.lwjgl.Version;
 
 public class Launcher {
     public static void main(String[] args) {
         System.out.println(Version.getVersion());
+        WindowManager window = new WindowManager("3D Game", 1600, 900, false);
+        window.init();
+
+        while (!window.windowShouldClose()) {
+            window.update();
+        }
+        
+        window.cleanup();
     }
 }
