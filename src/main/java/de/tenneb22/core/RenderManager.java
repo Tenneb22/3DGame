087b39e0d263332efd0199aa8e057c3ada1043e6
Tenneb22 <126985099+Tenneb22@users.noreply.github.com>
2024-09-28 21:44:48 +0200

    Basic Color

--- src/main/java/de/tenneb22/core/RenderManager.java
+++ src/main/java/de/tenneb22/core/RenderManager.java
@@ -1,6 +1,7 @@
 package de.tenneb22.core;
 
 import de.tenneb22.core.entity.Model;
+import de.tenneb22.core.utils.Utils;
 import de.tenneb22.test.Launcher;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
@@ -9,22 +10,28 @@ import org.lwjgl.opengl.GL30;
 public class RenderManager {
 
     private final WindowManager window;
+    private ShaderManager shader;
 
     public RenderManager() {
         window = Launcher.getWindow();
     }
 
     public void init() throws Exception {
-
+        shader = new ShaderManager();
+        shader.createVertexShader(Utils.loadResource("/shader/vertex.vs"));
+        shader.createFragmentShader(Utils.loadResource("/shader/fragment.fs"));
+        shader.link();
     }
 
     public void render(Model model) {
         clear();
+        shader.bind();
         GL30.glBindVertexArray(model.getId());
         GL20.glEnableVertexAttribArray(0);
         GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, model.getVertexCount());
         GL20.glDisableVertexAttribArray(0);
         GL30.glBindVertexArray(0);
+        shader.unbind();
     }
 
     public void clear() {
@@ -32,6 +39,6 @@ public class RenderManager {
     }
 
     public void cleanup() {
-
+        shader.cleanup();
     }
 }
--- src/main/java/de/tenneb22/core/utils/Utils.java
+++ src/main/java/de/tenneb22/core/utils/Utils.java
@@ -2,7 +2,11 @@ package de.tenneb22.core.utils;
 
 import org.lwjgl.system.MemoryUtil;
 
+import java.io.InputStream;
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Utils {
 
@@ -10,6 +14,22 @@ public class Utils {
         FloatBuffer buffer = MemoryUtil.memAllocFloat(data.length);
         buffer.put(data).flip();
         return buffer;
+    }
+
+    public static IntBuffer storeDataInIntBuffer(int[] data) {
+        IntBuffer buffer = MemoryUtil.memAllocInt(data.length);
+        buffer.put(data).flip();
+        return buffer;
+    }
+
+    public static String loadResource(String filename) throws Exception {
+        String result;
+        try(InputStream in = Utils.class.getResourceAsStream(filename)) {
+            try(Scanner scanner = new Scanner(in, StandardCharsets.UTF_8.name())) {
+                result = scanner.useDelimiter("\\A").next();
+            }
+        }
+        return result;
     }
 
 }
