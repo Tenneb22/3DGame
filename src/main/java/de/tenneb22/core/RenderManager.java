4dcd0445e2683768910108cfa1081fc2434fd7e0
Tenneb22 <126985099+Tenneb22@users.noreply.github.com>
2024-09-29 21:22:51 +0200

    Improved Rendering

--- src/main/java/de/tenneb22/core/RenderManager.java
+++ src/main/java/de/tenneb22/core/RenderManager.java
@@ -1,6 +1,7 @@
 package de.tenneb22.core;
 
 import de.tenneb22.core.entity.Entity;
+import de.tenneb22.core.entity.Model;
 import de.tenneb22.core.lighting.DirectionalLight;
 import de.tenneb22.core.lighting.PointLight;
 import de.tenneb22.core.lighting.SpotLight;
@@ -13,12 +14,19 @@ import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.lwjgl.opengl.GL11.glViewport;
 
 public class RenderManager {
 
     private final WindowManager window;
     private ShaderManager shader;
+
+    private Map<Model, List<Entity>> entities = new HashMap<>();
 
     public RenderManager() {
         window = Main.getWindow();
@@ -41,25 +49,33 @@ public class RenderManager {
         shader.createSpotLightListUniform("spotLights", 5);
     }
 
-    public void render(Entity entity, Camera camera, DirectionalLight directionalLight, PointLight[] pointLights, SpotLight[] spotLights) {
-        clear();
+    public void bind(Model model) {
+        GL30.glBindVertexArray(model.getId());
+        GL20.glEnableVertexAttribArray(0);
+        GL20.glEnableVertexAttribArray(1);
+        GL20.glEnableVertexAttribArray(2);
+        shader.setUniform("material", model.getMaterial());
+        GL13.glActiveTexture(GL13.GL_TEXTURE);
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, model.getTexture().getId());
+    }
 
-        if(window.isResize()) {
-            glViewport(0,0, window.getWidth(), window.getHeight());
-            window.setResize(false);
-        }
+    public void unbind() {
+        GL20.glDisableVertexAttribArray(0);
+        GL20.glDisableVertexAttribArray(1);
+        GL20.glDisableVertexAttribArray(2);
+        GL30.glBindVertexArray(0);
+    }
 
-        shader.bind();
-
+    public void prepare(Entity entity, Camera camera) {
         shader.setUniform("textureSampler", 0);
         shader.setUniform("transformationMatrix", Transformation.createTransformationMatrix(entity));
-        shader.setUniform("projectionMatrix", window.updateProjectionMatrix());
         shader.setUniform("viewMatrix", Transformation.getViewMatrix(camera));
-        shader.setUniform("material", entity.getModel().getMaterial());
+    }
+
+    public void renderLights(Camera camera, PointLight[] pointLights, SpotLight[] spotLights,
+                             DirectionalLight directionalLight) {
         shader.setUniform("ambientLight", Consts.AMBIENT_LIGHT);
         shader.setUniform("specularPower", Consts.SPECULAR_POWER);
-        shader.setUniform("directionalLight", directionalLight);
-
         int numLights = spotLights != null ? spotLights.length : 0;
         for(int i = 0; i < numLights; i++) {
             shader.setUniform("spotLights", spotLights[i], i);
@@ -69,21 +85,42 @@ public class RenderManager {
         for(int i = 0; i < numLights; i++) {
             shader.setUniform("pointLights", pointLights[i], i);
         }
+        shader.setUniform("directionalLight", directionalLight);
+    }
 
+    public void render(Camera camera, DirectionalLight directionalLight, PointLight[] pointLights, SpotLight[] spotLights) {
+        clear();
 
-        GL30.glBindVertexArray(entity.getModel().getId());
-        GL20.glEnableVertexAttribArray(0);
-        GL20.glEnableVertexAttribArray(1);
-        GL20.glEnableVertexAttribArray(2);
-        GL13.glActiveTexture(GL13.GL_TEXTURE);
-        GL11.glBindTexture(GL11.GL_TEXTURE_2D, entity.getModel().getTexture().getId());
-        GL11.glDrawElements(GL11.GL_TRIANGLES, entity.getModel().getVertexCount(), GL11.GL_UNSIGNED_INT, 0 );
-        GL20.glDisableVertexAttribArray(0);
-        GL20.glDisableVertexAttribArray(1);
-        GL20.glDisableVertexAttribArray(2);
-        GL30.glBindVertexArray(0);
+        if(window.isResize()) {
+            glViewport(0,0, window.getWidth(), window.getHeight());
+            window.setResize(false);
+        }
 
+        shader.bind();
+        shader.setUniform("projectionMatrix", window.updateProjectionMatrix());
+        renderLights(camera, pointLights, spotLights, directionalLight);
+        for(Model model : entities.keySet()) {
+            bind(model);
+            List<Entity> entityList = entities.get(model);
+            for (Entity entity : entityList) {
+                prepare(entity, camera);
+                GL11.glDrawElements(GL11.GL_TRIANGLES, entity.getModel().getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
+            }
+            unbind();
+        }
+        entities.clear();
         shader.unbind();
+    }
+
+    public void processEntity(Entity entity) {
+        List<Entity> entityList = entities.get(entity.getModel());
+        if(entityList != null)
+            entityList.add(entity);
+        else {
+            List<Entity> newEntityList = new ArrayList<>();
+            newEntityList.add(entity);
+            entities.put(entity.getModel(), newEntityList);
+        }
     }
 
     public void clear() {
--- src/main/java/de/tenneb22/test/TestGame.java
+++ src/main/java/de/tenneb22/test/TestGame.java
@@ -13,13 +13,17 @@ import org.joml.Vector3f;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL11;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class TestGame implements ILogic {
 
     private final RenderManager renderer;
     private final ObjectLoader loader;
     private final WindowManager window;
 
-    private Entity entity;
+    private List<Entity> entities;
     private Camera camera;
 
     Vector3f cameraInc;
@@ -44,7 +48,18 @@ public class TestGame implements ILogic {
 
         Model model = loader.loadOBJModel("/models/cube1.obj");
         model.setTexture(new Texture(loader.loadTexture("textures/blue.png")), 1f);
-        entity = new Entity(model, new Vector3f(0,0,-5), new Vector3f(0,0,0), 1);
+
+
+        entities = new ArrayList<>();
+        Random rnd = new Random();
+        for(int i = 0; i < 200; i++) {
+            float x = rnd.nextFloat() * 100 - 50;
+            float y = rnd.nextFloat() * 100 - 50;
+            float z = rnd.nextFloat() * -300;
+            entities.add(new Entity(model, new Vector3f(x,y,z),
+                    new Vector3f(rnd.nextFloat() * 100, rnd.nextFloat() * 180, 0), 1));
+        }
+        entities.add(new Entity(model, new Vector3f(0,0,-2f), new Vector3f(0,0,0), 1));
 
         float lightIntensity = 1.0f;
         //point light
@@ -137,11 +152,15 @@ public class TestGame implements ILogic {
         double angRad = Math.toRadians(lightAngle);
         directionalLight.getDirection().x = (float) Math.sin(angRad);
         directionalLight.getDirection().y = (float) Math.cos(angRad);
+
+        for(Entity entity : entities) {
+            renderer.processEntity(entity);
+        }
     }
 
     @Override
     public void render() {
-        renderer.render(entity, camera, directionalLight, pointLights, spotLights);
+        renderer.render(camera, directionalLight, pointLights, spotLights);
     }
 
     @Override
